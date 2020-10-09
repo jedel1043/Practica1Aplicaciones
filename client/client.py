d330a1b680da60abb4197845681842ce377304c8
jedel1043 <jedel0124@gmail.com>
2020-10-09 17:37:55 -0500

    feat: implementa envío de archivos usando sockets

--- client/client.py
+++ client/client.py
@@ -1,10 +1,55 @@
 import socket
+import sys
+import os
 
-HOST = 'localhost'
+HOST = socket.gethostname()
 PORT = 50007
+BUFFER_SIZE = 4096
 
+# abre un nuevo socket para conectarse con el servidor
+# socket.AF_INET = IPv4 socket
+# socket.SOCK_STREAM habilita enviar información particionada
+#   en arreglos de tamaño BUFFER_SIZE
+# "with" cierra el socket después al salir de su contexto
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-print('Received', repr(data))
+
+    # abre el archivo con ruta especificada en el argumento de entrada 1
+    # "with" cierra el archivo al salir de su contexto
+    # TODO: implementar envío de multiarchivos
+    with open(sys.argv[1], 'rb') as f:
+
+        abs_path = os.path.abspath(f.name)
+        rel_path = os.path.relpath(abs_path)
+        file_len = os.path.getsize(f.name)
+
+        print("Getting ready to send file \"" + rel_path
+              + "\" of", file_len, "bytes")
+
+        # sendall() no acepta str como argumento, solamente arreglos de bytes,
+        # por lo que la cadena "rel_path" se convierte a bytes
+        # utilizando la codificación de python por defecto (UTF-8)
+        s.sendall(rel_path.encode())
+
+        # sólo se envían 4 bytes (32 bits) de tamaño del archivo;
+        # el archivo a enviar no deberá superar los 2^32 - 1 bytes (approx. 4 GB)
+        s.sendall(file_len.to_bytes(4, byteorder="big"))
+
+        # Envía bytes hasta alcanzar el final del archivo (EOF)
+        byte_counter = 0
+        while byte_counter < file_len:
+            data = f.read(BUFFER_SIZE)
+            byte_counter += len(data)
+            
+            deliv = s.send(data)
+
+            # si "l" se envía incompleto, intenta enviar los bytes restantes 
+            # para evitar perder información
+            while (deliv < len(data)):
+                rem = s.send(data[deliv:])
+                deliv += rem
+
+            print("Received", len(data),
+                  "bytes (" + str(byte_counter * 100 // file_len) + "% completed)")
+        print()
+        print("Sent file \"" + rel_path + "\" to server")
--- server/server.py
+++ server/server.py
@@ -3,15 +3,57 @@ import socket
 
 HOST = ''
 PORT = 50007
+BUFFER_SIZE = 4096  # Recommended power of 2 and small buffer
+
 print('Starting server on port', PORT)
+
+# abre un nuevo socket para esperar peticiones de un cliente
+# socket.AF_INET = IPv4 socket
+# socket.SOCK_STREAM habilita recibir información particionada
+#   por arreglos de tamaño BUFFER_SIZE
+# with cierra automáticamente el socket después de salir
+#   de su contexto
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
-    print('Awaiting request')
+    print('Awaiting file')
+
+    # s.accept() espera alguna petición para
+    # continuar con la ejecución del programa
+    # conn: socket con la conexión entre el cliente
+    #       y el servidor
+    # addr: dirección ip donde se originó la conexión
     conn, addr = s.accept()
+
+    # with cierra la conexión al salir de su contexto
     with conn:
         print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data: break
-            conn.sendall(data)
+
+        # "recv" devuelve un arreglo de bytes que tenemos que convertir a str
+        # para poder obtener el nombre del archivo, por lo que se decodifica
+        # utilizando la codificación por defecto de python (UTF-8)
+        rel_path = conn.recv(BUFFER_SIZE).decode()
+
+        # sólo se reciben 4 bytes (32 bits) de tamaño del archivo;
+        # el archivo a recibir no deberá superar los 2^32 - 1 bytes (approx. 4 GB)
+        file_size = int.from_bytes(conn.recv(4), byteorder="big")
+
+        print("Receiving file \"" + rel_path + "\" of", file_size, "bytes")
+
+        # with cierra automáticamente el archivo después de salir de su contexto
+        with open(rel_path, "wb") as f:
+
+            # escribe bytes en el archivo de salida hasta recibir el
+            # último byte
+            byte_counter = 0
+            while byte_counter < file_size:
+                temp_data = conn.recv(BUFFER_SIZE)
+                byte_counter += len(temp_data)
+                f.write(temp_data)
+                
+                print("Received", len(temp_data),
+                      "bytes ("
+                      + str(byte_counter * 100 // file_size)
+                      + "% completed)")
+        print()
+        print("Wrote file \"" + rel_path + "\" to pwd")
