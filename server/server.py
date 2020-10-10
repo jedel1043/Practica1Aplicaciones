550b66b4fd708a6485587248134ad25e465afeab
jedel1043 <jedel0124@gmail.com>
2020-10-10 13:00:10 -0500

    feat: agrega soporte para multiarchivos y carpetas

--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
 # Echo server program
 import socket
+import os
+import pathlib
 
 HOST = ''
 PORT = 50007
@@ -16,44 +18,57 @@ print('Starting server on port', PORT)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
-    print('Awaiting file')
 
-    # s.accept() espera alguna petición para
-    # continuar con la ejecución del programa
-    # conn: socket con la conexión entre el cliente
-    #       y el servidor
-    # addr: dirección ip donde se originó la conexión
-    conn, addr = s.accept()
+    while (True):
+        # s.accept() espera alguna petición para
+        # continuar con la ejecución del programa
+        # conn: socket con la conexión entre el cliente
+        #       y el servidor
+        # addr: dirección ip donde se originó la conexión
+        print('Awaiting request')
+        conn, addr = s.accept()
 
-    # with cierra la conexión al salir de su contexto
-    with conn:
-        print('Connected by', addr)
+        # with cierra la conexión al salir de su contexto
+        with conn:
+            print('Connected by', addr)
 
-        # "recv" devuelve un arreglo de bytes que tenemos que convertir a str
-        # para poder obtener el nombre del archivo, por lo que se decodifica
-        # utilizando la codificación por defecto de python (UTF-8)
-        rel_path = conn.recv(BUFFER_SIZE).decode()
+            # se reciven 2 bytes (16 bits) de tamaño de ruta del archivo;
+            # la longitud de ruta no deberá superar los 2^16 - 1 bytes (65536 bytes)
+            pathlen = int.from_bytes(conn.recv(2), byteorder="big")
 
-        # sólo se reciben 4 bytes (32 bits) de tamaño del archivo;
-        # el archivo a recibir no deberá superar los 2^32 - 1 bytes (approx. 4 GB)
-        file_size = int.from_bytes(conn.recv(4), byteorder="big")
+            # "recv" devuelve un arreglo de bytes que se decodifica a "str"
+            # para obtener el nombre del archivo utilizando la codificación 
+            # por defecto de python (UTF-8)
+            relpath = conn.recv(pathlen).decode()
+            relpath = os.path.join(addr[0], relpath)
 
-        print("Receiving file \"" + rel_path + "\" of", file_size, "bytes")
+            dirname, basename = os.path.split(relpath)
+            
+            if dirname and not os.path.exists(dirname):
+                pathlib.Path(dirname).mkdir(parents=True)
+                print("Created directory \"" + dirname + "\"")
 
-        # with cierra automáticamente el archivo después de salir de su contexto
-        with open(rel_path, "wb") as f:
+            if basename:
+                # se reciben 4 bytes (32 bits) de tamaño del archivo;
+                # el archivo a recibir no deberá superar los 2^32 - 1 bytes (approx. 4 GB)
+                filelen = int.from_bytes(conn.recv(4), byteorder="big")
 
-            # escribe bytes en el archivo de salida hasta recibir el
-            # último byte
-            byte_counter = 0
-            while byte_counter < file_size:
-                temp_data = conn.recv(BUFFER_SIZE)
-                byte_counter += len(temp_data)
-                f.write(temp_data)
-                
-                print("Received", len(temp_data),
-                      "bytes ("
-                      + str(byte_counter * 100 // file_size)
-                      + "% completed)")
-        print()
-        print("Wrote file \"" + rel_path + "\" to pwd")
+                print("Receiving file \"" + relpath + "\" of", filelen, "bytes")
+
+                # with cierra automáticamente el archivo después de salir de su contexto
+                with open(relpath, "wb") as f:
+
+                    # escribe bytes en el archivo de salida hasta recibir el
+                    # último byte
+                    byte_counter = 0
+                    while byte_counter < filelen:
+                        temp_data = conn.recv(BUFFER_SIZE)
+                        byte_counter += len(temp_data)
+                        f.write(temp_data)
+
+                        print("Received", len(temp_data),
+                            "bytes ("
+                            + str(byte_counter * 100 // filelen)
+                            + "% completed)")
+                print("Wrote file \"" + relpath + "\"")
+                print()
