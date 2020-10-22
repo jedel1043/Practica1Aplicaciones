a24e09f8ad42e56e028dc6142b1a5fadb235c997
jedel1043 <jedel0124@gmail.com>
2020-10-22 08:14:19 -0500

    feat: implementa envío de archivos en una conexión

--- client/client.py
+++ client/client.py
@@ -1,15 +1,15 @@
-from os.path import relpath
 import socket
 import sys
 import os
-from typing import overload
+from typing import List, Tuple
 
 # info del servidor destino
 HOST = socket.gethostname()
 PORT = 50007
 BUFFER_SIZE = 4096
 
-''' Formato para el envío de un archivo:
+''' Formato para el envío de archivos:
+    2 bytes: número de archivos a recibir
     2 bytes: longitud de la ruta destino
     n bytes: ruta destino
     4 bytes: longitud del archivo
@@ -17,12 +17,10 @@ BUFFER_SIZE = 4096
 '''
 
 '''
-    filepath:   ruta del archivo a enviar
-    relpath:    ruta que tendrá el archivo al recibirse en el servidor
+    paths:   List[Tuple[rutas de los archivos a enviar, rutas que tendrán los archivos al recibirse en el servidor]]
 '''
 
-
-def sendfile(filepath, relpath):
+def sendfiles(paths: List[Tuple[str, str]]):
     # abre un nuevo socket para conectarse con el servidor
     # socket.AF_INET = IPv4 socket
     # socket.SOCK_STREAM habilita el protocolo TCP
@@ -30,47 +28,57 @@ def sendfile(filepath, relpath):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
 
-        # abre el archivo con ruta "filepath"
-        # "with" cierra el archivo al salir de su contexto
-        with open(filepath, 'rb') as f:
-            pathlen = len(relpath)
-            filelen = os.path.getsize(f.name)
+        # se reciben 2 bytes (16 bits) con el número de archivos a recibir;
+        # el número de archivos no deberá superar los 2^16 - 1 bytes (65536 bytes)
+        print(f"Sending to server the number of files to send \"{len(paths)}\"")
+        s.sendall(len(paths).to_bytes(2, byteorder="big"))
 
-            print("Getting ready to send file \"" + filepath
-                  + "\" of", filelen, "bytes")
+        for filepath, relpath in paths:
+            # abre el archivo con ruta "filepath"
+            # "with" cierra el archivo al salir de su contexto
+            with open(filepath, 'rb') as f:
+                pathlen = len(relpath)
+                filelen = os.path.getsize(f.name)
 
-            # se envían 2 bytes (16 bits) de tamaño de ruta del archivo;
-            # la longitud de ruta no deberá superar los 2^16 - 1 bytes (65536 bytes)
-            s.sendall(pathlen.to_bytes(2, byteorder="big"))
+                print("Getting ready to send file \"" + filepath
+                    + "\" of", filelen, "bytes")
 
-            # sendall() no acepta str como argumento, solamente arreglos de bytes,
-            # por lo que la cadena "relpath" se convierte a bytes
-            # utilizando la codificación de python por defecto (UTF-8)
-            s.sendall(relpath.encode())
+                # envía 2 bytes (16 bits) de tamaño de ruta del archivo;
+                # la longitud de ruta no deberá superar los 2^16 - 1 bytes (65536 bytes)
+                s.sendall(pathlen.to_bytes(2, byteorder="big"))
 
-            # se envían 4 bytes (32 bits) de tamaño del archivo;
-            # el archivo a enviar no deberá superar los 2^32 - 1 bytes (approx. 4 GB)
-            s.sendall(filelen.to_bytes(4, byteorder="big"))
+                # sendall() no acepta str como argumento, solamente arreglos de bytes,
+                # por lo que la cadena "relpath" se convierte a bytes
+                # utilizando la codificación de python por defecto (UTF-8)
+                s.sendall(relpath.encode())
 
-            # Envía bytes hasta alcanzar el final del archivo (EOF)
-            byte_counter = 0
-            while byte_counter < filelen:
-                data = f.read(BUFFER_SIZE)
-                byte_counter += len(data)
+                # envía 4 bytes (32 bits) de tamaño del archivo;
+                # el archivo a enviar no deberá superar los 2^32 - 1 bytes (approx. 4 GB)
+                s.sendall(filelen.to_bytes(4, byteorder="big"))
 
-                delivered = s.send(data)
+                # envía bytes hasta alcanzar el final del archivo (EOF)
+                byte_counter = 0
+                while byte_counter < filelen:
+                    data = f.read(BUFFER_SIZE)
+                    byte_counter += len(data)
 
-                # si "data" se envía incompleto, intenta enviar los bytes restantes
-                # para evitar perder información
-                while (delivered < len(data)):
-                    rem = s.send(data[delivered:])
-                    delivered += rem
+                    delivered = s.send(data)
 
-                print("Sent", len(data),
-                      "bytes (" + str(byte_counter * 100 // filelen) + "% completed)")
-            print("Correcly sent file \"" + filepath + "\" to server")
-            print()
+                    # si "data" se envía incompleto, intenta enviar los bytes restantes
+                    # para evitar perder información
+                    while (delivered < len(data)):
+                        rem = s.send(data[delivered:])
+                        delivered += rem
 
+                    print("Sent", len(data),
+                        "bytes (" + str(byte_counter * 100 // filelen) + "% completed)")
+                print("Correcly sent file \"" + filepath + "\" to server")
+                print()
+
+
+# guarda todas las rutas relativas y absolutas
+# de los archivos a enviar al servidor
+paths = []
 
 # obtenemos todos los argumentos del script
 # excepto el nombre del mismo script
@@ -91,13 +99,16 @@ for arg in sys.argv[1:]:
                 newpath = os.path.join(
                     dirname, os.path.relpath(filepath, start=directory))
 
-                sendfile(filepath, newpath)
+                paths.append((filepath, newpath))
 
     # si es un archivo, el servidor no crea una carpeta
     elif os.path.isfile(arg):
-        sendfile(arg, os.path.basename(arg))
+        paths.append((arg, os.path.basename(arg)))
 
     # ignora argumentos de entrada inválidos para
     # procesar todos los archivos posibles
     else:
         print("path not found")
+
+# realiza el envío de los archivos
+sendfiles(paths)
--- server/server.py
+++ server/server.py
@@ -8,7 +8,8 @@ PORT = 50007
 BUFFER_SIZE = 4096  # buffer óptimo con tamaño pequeño y potencia de 2
 
 
-''' Formato para la recepción de un archivo:
+''' Formato para la recepción de archivos:
+    2 bytes: número de archivos a recibir
     2 bytes: longitud de la ruta destino (pathlen)
     n bytes: ruta destino
     4 bytes: longitud del archivo
@@ -39,43 +40,49 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         with conn:
             print('Connected by', addr)
 
-            # se reciven 2 bytes (16 bits) de tamaño de ruta del archivo;
-            # la longitud de ruta no deberá superar los 2^16 - 1 bytes (65536 bytes)
-            pathlen = int.from_bytes(conn.recv(2), byteorder="big")
+            # se reciben 2 bytes (16 bits) con el número de archivos a recibir;
+            # el número de archivos no deberá superar los 2^16 - 1 bytes (65536 bytes)
+            filen = int.from_bytes(conn.recv(2), byteorder="big")
 
-            # "recv" devuelve un arreglo de bytes que se decodifica a "str"
-            # para obtener el nombre del archivo utilizando la codificación 
-            # por defecto de python (UTF-8)
-            relpath = conn.recv(pathlen).decode()
-            relpath = os.path.join(addr[0], relpath)
+            for unused in range(filen):
 
-            dirname, basename = os.path.split(relpath)
-            
-            if dirname and not os.path.exists(dirname):
-                pathlib.Path(dirname).mkdir(parents=True)
-                print("Created directory \"" + dirname + "\"")
+                # se reciben 2 bytes (16 bits) de tamaño de ruta del archivo;
+                # la longitud de ruta no deberá superar los 2^16 - 1 bytes (65536 bytes)
+                pathlen = int.from_bytes(conn.recv(2), byteorder="big")
 
-            if basename:
-                # se reciben 4 bytes (32 bits) de tamaño del archivo;
-                # el archivo a recibir no deberá superar los 2^32 - 1 bytes (approx. 4 GB)
-                filelen = int.from_bytes(conn.recv(4), byteorder="big")
+                # "recv" devuelve un arreglo de bytes que se decodifica a "str"
+                # para obtener el nombre del archivo utilizando la codificación 
+                # por defecto de python (UTF-8)
+                relpath = conn.recv(pathlen).decode()
+                relpath = os.path.join(addr[0], relpath)
 
-                print("Receiving file \"" + relpath + "\" of", filelen, "bytes")
+                dirname, basename = os.path.split(relpath)
+                
+                if dirname and not os.path.exists(dirname):
+                    pathlib.Path(dirname).mkdir(parents=True)
+                    print("Created directory \"" + dirname + "\"")
 
-                # with cierra automáticamente el archivo después de salir de su contexto
-                with open(relpath, "wb") as f:
+                if basename:
+                    # se reciben 4 bytes (32 bits) de tamaño del archivo;
+                    # el archivo a recibir no deberá superar los 2^32 - 1 bytes (approx. 4 GB)
+                    filelen = int.from_bytes(conn.recv(4), byteorder="big")
 
-                    # escribe bytes en el archivo de salida hasta recibir el
-                    # último byte
-                    byte_counter = 0
-                    while byte_counter < filelen:
-                        temp_data = conn.recv(BUFFER_SIZE)
-                        byte_counter += len(temp_data)
-                        f.write(temp_data)
+                    print("Receiving file \"" + relpath + "\" of", filelen, "bytes")
 
-                        print("Received", len(temp_data),
-                            "bytes ("
-                            + str(byte_counter * 100 // filelen)
-                            + "% completed)")
-                print("Wrote file \"" + relpath + "\"")
-                print()
+                    # with cierra automáticamente el archivo después de salir de su contexto
+                    with open(relpath, "wb") as f:
+
+                        # escribe bytes en el archivo de salida hasta recibir el
+                        # último byte
+                        byte_counter = 0
+                        while byte_counter < filelen:
+                            temp_data = conn.recv(BUFFER_SIZE if (rem := filelen - byte_counter) > BUFFER_SIZE else rem)
+                            byte_counter += len(temp_data)
+                            f.write(temp_data)
+
+                            print("Received", len(temp_data),
+                                "bytes ("
+                                + str(byte_counter * 100 // filelen)
+                                + "% completed)")
+                    print("Wrote file \"" + relpath + "\"")
+                    print()
