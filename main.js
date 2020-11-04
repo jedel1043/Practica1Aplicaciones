95e32a755cd7e2c7857ceebec376ab2d897e3b32
RicentCB <ricardo.cb009@gmail.com>
2020-11-04 21:37:07 +0100

    Preparacion para conexion

--- main.js
+++ main.js
@@ -1,31 +1,19 @@
 const {app, BrowserWindow} = require('electron');
 
 function createWindow() {
-    const win = new BrowserWindow({
+    let win = new BrowserWindow({
         width: 950,
         height: 600,
+        resizable: false,
         webPreferences: {
             nodeIntegration: true
-        },
-        resizable: false
+        }
     });
 
     win.loadFile('index.html');
-    // win.removeMenu();
+    win.removeMenu();
 
     // win.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow);
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
-    }
-})
+app.on("ready", createWindow);
