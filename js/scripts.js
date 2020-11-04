325f1051c27f809130cc05b5e5d07d498e765cb6
RicentCB <ricardo.cb009@gmail.com>
2020-11-04 21:30:03 +0100

    Primera Conexion Con python Shell

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
+var path = require("path");
 
-
-// var path = require("path");
-// var optionsPython = {
-//     mode: 'json',
-//     pythonOptions: ['-u'],
-//     scriptPath: path.join(__dirname, 'Engine/'),
-//     // pythonPath: '/usr/bin/python3',         //Cambiar la ruta de acuerdo al sistema
-//     args: []
-// };
+var optionsPython = {
+    // mode: 'json',
+    pythonOptions: ['-u'],
+    scriptPath: path.join(__dirname, 'Engine/'),
+    // pythonPath: '/usr/bin/python3',         //Cambiar la ruta de acuerdo al sistema
+    args: []
+};
 
 $(document).ready(function(){
-    // const { PythonShell } = require("python-shell");
-    // optionsPython.args = ["arg1", "arg2", "arg3"];
+    const { PythonShell } = require("python-shell");
+    optionsPython.args = ["arg1", "arg2", "arg3"];
 
-    // let pythonScript = new PythonShell("hello.py", optionsPython);
+    let pythonScript = new PythonShell("hello.py", optionsPython);
 
-    // pythonScript.on('message', function(out){
-    //     console.log(out);
-    // })
-    console.log("Hola");
+    pythonScript.on('message', function(out){
+        console.log(out);
+    })
+    console.log("Hola mundo");
 })
