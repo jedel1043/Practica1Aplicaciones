fd1bd79ee825fa874264ce21f8f05de0891b43e1
RicentCB <ricardo.cb009@gmail.com>
2020-11-04 22:06:05 +0100

    Integracion de Usuario

---
+++ js/login.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    const LoginWindow = $(".login");
+
+    LoginWindow.on("click", "#submitBtn", function(e){
+        e.preventDefault();
+
+        let username = LoginWindow.find("#userInput").val();
+        console.log(username);
+        window.location = "upload.html?username="+username;
+    })
+})
